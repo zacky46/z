ee7b57862af181939cb6a4f6b697324be7995e40
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-09-23 05:41:35 +0700

    Test

--- build.sh
+++ build.sh
@@ -17,3 +17,4 @@ mka evolution -j8
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip NFS:$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
