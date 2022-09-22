6b74efe5b561ec1767cc6aae87bb5d01aec83a18
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-09-23 05:10:01 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Builder-CI
+
+Test build any Rom A11 only
+
+[![Build Status](https://api.cirrus-ci.com/github/NFS86/Builder-CI.svg)](https://cirrus-ci.com/github/NFS86/Builder-CI)
---
+++ build.sh
@@ -0,0 +1,21 @@
+#sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://github.com/NFS-project/local_manifest --depth 1 -b rosy-evox-13 .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build rom
+source build/envsetup.sh
+lunch evolution_rosy-userdebug
+export TZ=Asia/Jakarta
+export KBUILD_BUILD_USER=rosy
+export KBUILD_BUILD_HOST=nfsproject
+export BUILD_USERNAME=rosy
+export BUILD_HOSTNAME=nfsproject
+export LINUX_COMPILE_BY=NFS86
+export LINUX_COMPILE_HOST=NFS-project
+export use_GrapheneCamera=true
+export USE_NFSparts=true
+mka evolution -j8
+
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip NFS:$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
