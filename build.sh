d5a1ddb2288d1767a45f5730b1448875d51c0bb4
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-09-23 08:43:54 +0700

    [skip ci]

--- build.sh
+++ build.sh
@@ -14,7 +14,8 @@ export BUILD_HOSTNAME=nfsproject
 export use_GrapheneCamera=true
 export USE_NFSparts=true
 mka evolution -j8
+# end
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+# upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip NFS:$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 
