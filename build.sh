af333fb7b736c5f743c4f67f9c6102dd285d63e7
NFS86 <jarbull86@gmail.com>
2022-10-12 22:59:20 +0000

    update to v7.2

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS-project/local_manifest --depth 1 -b rosy-derp-13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://github.com/NFS-project/local_manifest --depth 1 -b rosy-evox-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-export NFS_TARGET_BUILD_ROM=DerpFest
+export NFS_TARGET_BUILD_ROM=EvolutionX
 source build/envsetup.sh
-lunch derp_rosy-userdebug
+lunch evolution_rosy-userdebug
 export TZ=Asia/Jakarta
 export KBUILD_BUILD_USER=rosy
 export KBUILD_BUILD_HOST=nfsproject
 export BUILD_USERNAME=rosy
 export BUILD_HOSTNAME=nfsproject
 export USE_NFSparts=true
-mka derp -j8
+mka evolution -j8
 # end
