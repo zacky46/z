5f519397d3326a5896fb4cbf7a4e60039e06fb36
zacky <103025109+zacky46@users.noreply.github.com>
2022-10-14 23:37:48 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,16 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS-project/local_manifest --depth 1 -b rosy-evox-13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/NFS-project/local_manifest --depth 1 -b banana-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-export NFS_TARGET_BUILD_ROM=EvolutionX
+export NFS_TARGET_BUILD_ROM=bananadroid
 source build/envsetup.sh
-lunch evolution_rosy-userdebug
+lunch banana_ginkgo-user
 export TZ=Asia/Jakarta
-export KBUILD_BUILD_USER=rosy
+export KBUILD_BUILD_USER=zacky
 export KBUILD_BUILD_HOST=nfsproject
-export BUILD_USERNAME=rosy
+export BUILD_USERNAME=zacky
 export BUILD_HOSTNAME=nfsproject
-export USE_NFSparts=true
-mka evolution -j8
+m banana
 # end
