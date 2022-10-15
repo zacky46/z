1f555691ed60f16de9c01dabacf716678bcf58de
c3eru <96016081+c3eru@users.noreply.github.com>
2022-10-15 10:23:27 +0700

    ci: update [skip ci]

--- script/ziping.sh
+++ script/ziping.sh
@@ -35,7 +35,7 @@ cipher=$(ls out/target/product/$device/CipherOS-*-OTA-*.zip || true)
 rm -rf $engzip $otazip $awaken $octavi $p404 $cipher
 file_name=$(basename out/target/product/$device/*.zip)
 DL_LINK=https://nfsproject.projek.workers.dev/0:/$name_rom/$device/$file_name
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip NFS:$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip mobx:$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)/$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 cd $WORKDIR/rom/$name_rom/out/target/product/$device
 echo -e \
 "
@@ -78,7 +78,7 @@ com ()
   tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
 }
 time com ccache 1
-rclone copy --drive-chunk-size 256M --stats 1s ccache.tar.gz NFS:ccache/$name_rom -P
+rclone copy --drive-chunk-size 256M --stats 1s ccache.tar.gz mobx:ccache/$name_rom -P
 rm -rf ccache.tar.gz
 echo ━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
 msg Upload ccache succes..
