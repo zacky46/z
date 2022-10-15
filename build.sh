aa231a25e2b69297a9e5259eca72ead7a54ea836
c3eru <96016081+c3eru@users.noreply.github.com>
2022-10-15 10:13:11 +0700

    ci: cleanup build.sh [skip ci]

--- build.sh
+++ build.sh
@@ -1,16 +1,15 @@
 #sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS-project/local_manifest --depth 1 -b banana-12 .repo/local_manifests
+git clone https://github.com/MOBX-PROJECTS/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-export NFS_TARGET_BUILD_ROM=bananadroid
 source build/envsetup.sh
-lunch banana_ginkgo-user
+lunch banana_spes-user
 export TZ=Asia/Jakarta
-export KBUILD_BUILD_USER=zacky
+export KBUILD_BUILD_USER=mobx
 export KBUILD_BUILD_HOST=nfsproject
-export BUILD_USERNAME=zacky
+export BUILD_USERNAME=mobx
 export BUILD_HOSTNAME=nfsproject
 mka banana -j8
 # end 
