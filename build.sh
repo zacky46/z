b468cdc167309460249370e84adb01a29f75977d
c3eru <96016081+c3eru@users.noreply.github.com>
2022-10-16 17:03:13 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/MOBX-PROJECTS/local_manifest --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://github.com/MOBX-PROJECTS/local_manifest --depth 1 -b evox .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
@@ -8,14 +8,14 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # build rom
 source build/envsetup.sh
-lunch banana_spes-user
+lunch evolution_spes-userdebug
 export TZ=Asia/Jakarta
 export KBUILD_BUILD_USER=mobx
 export KBUILD_BUILD_HOST=nfsproject
 export BUILD_USERNAME=mobx
 export BUILD_HOSTNAME=nfsproject
 export WITH_GAPPS=true
-mka banana -j8
+mka evolution -j8
 # end 
 
 # build 6
