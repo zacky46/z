ae3efa1c91953c20df866bc087e563c034b1f9bb
c3eru <96016081+c3eru@users.noreply.github.com>
2022-10-18 20:46:30 +0700

    Test

--- build.sh
+++ build.sh
@@ -1,21 +1,17 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/MOBX-PROJECTS/local_manifest --depth 1 -b evox .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 10 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/MOBX-PROJECTS/local_manifest --depth 1 -b nad .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
-
-rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # build rom
 source build/envsetup.sh
-lunch evolution_spes-userdebug
+lunch nad_juice-userdebug
 export TZ=Asia/Jakarta
 export KBUILD_BUILD_USER=mobx
 export KBUILD_BUILD_HOST=nfsproject
 export BUILD_USERNAME=mobx
 export BUILD_HOSTNAME=nfsproject
-export WITH_GAPPS=true
-mka evolution -j8
+mka nad -j8
 # end 
 
-# build 6
+# build 7
