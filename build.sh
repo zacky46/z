8aa2f2fd188f3c0bf26be2cf9f7acef94505300f
c3eru <96016081+c3eru@users.noreply.github.com>
2022-10-18 21:09:45 +0700

    Test private

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 10 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/MOBX-PROJECTS/local_manifest --depth 1 -b nad .repo/local_manifests
+git clone https://github.com/c3eru/local_manifest --depth 1 -b nad .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -14,4 +14,4 @@ export BUILD_HOSTNAME=nfsproject
 mka nad -j8
 # end 
 
-# build 7
+# build 8
