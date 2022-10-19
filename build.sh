0408568142894765027a45cfb4101ba1ed516cdb
c3eru <96016081+c3eru@users.noreply.github.com>
2022-10-19 16:54:58 +0700

    Gapps trial

--- build.sh
+++ build.sh
@@ -8,10 +8,11 @@ source build/envsetup.sh
 lunch nad_juice-userdebug
 export TZ=Asia/Jakarta
 export KBUILD_BUILD_USER=mobx
-export KBUILD_BUILD_HOST=nfsproject
+export KBUILD_BUILD_HOST=builder-ci-task-kang-moment
 export BUILD_USERNAME=mobx
-export BUILD_HOSTNAME=nfsproject
-export WITH_GAPPS=true
+export BUILD_HOSTNAME=builder-ci-task-kang-moment
+export USE_MICROG=true
+export USE_GAPPS=true
 mka nad -j8
 # end 
 
