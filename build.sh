fb9683771c85560b152ec0d09b8aee88551081b8
yanzszs <115848467+yanzszs@users.noreply.github.com>
2022-10-20 16:58:03 +0700

    build: NusantaraProject Android 10
    * build: nusantara project android 10 ginkgo

--- build.sh
+++ build.sh
@@ -1,19 +1,17 @@
 #sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 10 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/c3eru/local_manifest --depth 1 -b nad .repo/local_manifests
+git clone https://github.com/yanzszs/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch nad_juice-userdebug
+lunch nad_ginkgo-userdebug
 export TZ=Asia/Jakarta
-export KBUILD_BUILD_USER=mobx
-export KBUILD_BUILD_HOST=builder-ci-task-kang-moment
-export BUILD_USERNAME=mobx
-export BUILD_HOSTNAME=builder-ci-task-kang-moment
+export KBUILD_BUILD_USER=yanzszs
+export KBUILD_BUILD_HOST=cirrus
+export BUILD_USERNAME=yanzszs
+export BUILD_HOSTNAME=cirrus
 # export USE_MICROG=true
 export USE_GAPPS=true
 mka nad -j8
-# end 
-
-# build 9
+# end
