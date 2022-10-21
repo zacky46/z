eac705ec35158dc15346971b04596df135393c50
yanzszs <115848467+yanzszs@users.noreply.github.com>
2022-10-22 03:12:21 +0700

    Setup build [skip ci]

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 10 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/yanzszs/local_manifest --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://github.com/yanzszs/local_manifest --depth 1 -b whyred-evo-x-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch nad_ginkgo-userdebug
+lunch evolution_whyred-userdebug
 export TZ=Asia/Jakarta
-export KBUILD_BUILD_USER=yanzszs
+export KBUILD_BUILD_USER=zacky
 export KBUILD_BUILD_HOST=cirrus
-export BUILD_USERNAME=yanzszs
+export BUILD_USERNAME=zacky
 export BUILD_HOSTNAME=cirrus
 # export USE_MICROG=true
 export USE_GAPPS=true
-mka nad -j8
+mka evolution -j8
 # end 
